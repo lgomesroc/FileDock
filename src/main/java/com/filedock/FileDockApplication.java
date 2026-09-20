5907dd3ffb2a645621a82b737ae8a4e1785f19dd
lgomesroc <lgomesroc2012@gmail.com>
2026-09-20 09:15:43 -0300

    chore: initialize FileDock project

---
+++ src/main/java/com/filedock/FileDockApplication.java
@@ -0,0 +1,13 @@
+package com.filedock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileDockApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FileDockApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/filedock/FileDockApplicationTests.java
@@ -0,0 +1,13 @@
+package com.filedock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FileDockApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
